94b3604175eb2453904bbc099202267f35d52828
bthomas2b <bhthomas@andrew.cmu.edu>
2015-03-02 22:28:50 -0500

    child controls
    movement (arrow keys), jumping (space), climbing(up arrow), grabbing
    objects (hold left control)

---
+++ PlatformerGame/Assets/UnityAssets/2D/Scripts/Grab.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+
+	public bool grabbed = false;
+	public HingeJoint2D hinge;
+	public Animator anim;
+	public Transform child;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftControl)) {
+			if (Vector3.Distance (child.position, transform.renderer.bounds.center) < 1.5f && anim.GetBool("Crouch") == false) {
+					grabbed = true;
+					hinge.enabled = true;
+			} //bind the object to the player
+		}
+		else { 
+			grabbed = false;
+			hinge.enabled = false;} //let go
+		anim.SetBool("Grab", grabbed);
+		}
+	}
---
+++ PlatformerGame/Assets/UnityAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+
+            if (!jump)
+            // Read the jump input in Update so button presses aren't missed.
+			jump = Input.GetKey (KeyCode.Space);
+
+        }
+
+        private void FixedUpdate()
+        {
+
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.DownArrow);
+            float h = 0;
+			if (Input.GetKey(KeyCode.LeftArrow)){ h = -0.35f;}
+			if (Input.GetKey(KeyCode.RightArrow)){ h = 0.35f;}
+			if (Input.GetKey (KeyCode.UpArrow)) {character.Climb();}
+            // Pass all parameters to the character control script.
+            character.Move(h, crouch, jump);
+            jump = false;
+        }
+    }
+}
