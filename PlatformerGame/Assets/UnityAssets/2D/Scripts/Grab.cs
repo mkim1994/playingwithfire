795ba86b4d09491a431214e7915fa6d97ae6832e
bthomas2b <bhthomas@andrew.cmu.edu>
2015-03-19 21:16:11 -0400

    fixed walls again (DONT SET IT TO BOUNCY BOX)
    no more grab errror (make sure you point to child and anim of child in
    the inspector)

--- PlatformerGame/Assets/UnityAssets/2D/Scripts/Grab.cs
+++ PlatformerGame/Assets/UnityAssets/2D/Scripts/Grab.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class Grab : MonoBehaviour {
 
 	public bool grabbed = false;
-	public HingeJoint2D hinge;
 	public Animator anim;
 	public Transform child;
 
 	// Update is called once per frame
 	void Update () {
+		HingeJoint2D hinge = gameObject.GetComponent<HingeJoint2D>();
 		if (Input.GetKey (KeyCode.LeftControl)) {
 			if (Vector3.Distance (child.position, transform.renderer.bounds.center) < 1.5f && anim.GetBool("Crouch") == false) {
 					grabbed = true;
