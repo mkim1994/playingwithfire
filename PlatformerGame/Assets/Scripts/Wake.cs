0c0031dade8fab1a854f45dbc01223eaf30f44d4
bthomas2b <bhthomas@andrew.cmu.edu>
2015-03-27 10:01:53 -0400

    some more changes to level 3

---
+++ PlatformerGame/Assets/Scripts/Wake.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wake : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll){
+
+		if (coll.gameObject.tag == "Player"){
+			this.rigidbody2D.fixedAngle = false;
+		}
+	}
+}
