86a8c4110b505d97a1bfff1ab42ab2643051dd22
mkim1994 <mkim2@andrew.cmu.edu>
2015-03-27 04:41:54 -0400

    start screen, pls fix button

---
+++ PlatformerGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		//Application.LoadLevel ("Intro");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void nextLevel(){
+		nextlvl();
+
+	}
+
+	void nextlvl(){
+		Application.LoadLevel ("Intro");
+	}
+}
