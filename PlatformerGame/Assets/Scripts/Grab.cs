568d3412057efbb6d2748438b50abb3d50cefb12
bthomas2b <bhthomas@andrew.cmu.edu>
2015-03-26 22:30:27 -0400

    only grab one thing

--- PlatformerGame/Assets/Scripts/Grab.cs
+++ PlatformerGame/Assets/Scripts/Grab.cs
@@ -11,14 +11,16 @@ public class Grab : MonoBehaviour {
 	void Update () {
 		HingeJoint2D hinge = gameObject.GetComponent<HingeJoint2D>();
 		if (Input.GetKey (KeyCode.LeftControl)) {
-			if (Vector3.Distance (child.position, transform.renderer.bounds.center) < 1.5f && anim.GetBool("Crouch") == false) {
-					grabbed = true;
-					hinge.enabled = true;
+			if (Vector3.Distance (child.position, transform.renderer.bounds.center) < 1.5f && !anim.GetBool("Crouch") && !anim.GetBool("Grab")) {
+				grabbed = true;
+				hinge.enabled = true;
+
 			} //bind the object to the player
 		}
 		else { 
 			grabbed = false;
-			hinge.enabled = false;} //let go
+			hinge.enabled = false;
+		} //let go
 		anim.SetBool("Grab", grabbed);
-		}
 	}
+}
