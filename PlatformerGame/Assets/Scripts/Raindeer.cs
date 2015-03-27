13a2ff099afc8e64c9d4ccb14d1f4ba06f981900
aasahay <a.amal.sahay@gmail.com>
2015-03-27 03:08:18 -0400

    Adjusting the Reindeer collider

--- PlatformerGame/Assets/Scripts/Raindeer.cs
+++ PlatformerGame/Assets/Scripts/Raindeer.cs
@@ -35,7 +35,7 @@ namespace UnitySampleAssets._2D
 				}
 				else if((mounted) && (child.rigidbody2D.velocity.y == 0)){ //dismount
 					GetComponent<SpriteRenderer>().renderer.enabled = true; //hide the raindeer
-					transform.position = child.position + new Vector3(0,0.7f,0); //appear at mounted childs position
+					transform.position = child.position + new Vector3(0,0.4f,0); //appear at mounted childs position
 					//transform.position+= new Vector3(0,10,0);
 					childscript.Dismount();
 					mounted = false;
